df9f8eb1826b226826b8dd2117740d184d4d10c5
Cian Hughes <chughes000@gmail.com>
2026-02-19 14:27:26 +0000

    Fixed deprecated call in nvim-treesitter config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -80,7 +80,7 @@ return {
         },
         config = function(_, opts)
             ---@diagnostic disable-next-line: missing-fields
-            require("nvim-treesitter.configs").setup(opts)
+            require("nvim-treesitter.config").setup(opts)
         end,
     },
 }

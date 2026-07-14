7a53723fb605e7438bd31f6d17e7269147b55c4c
Cian Hughes <chughes000@gmail.com>
2026-07-14 15:03:24 +0100

    Fixed fennel initialisation problems

--- init.lua
+++ init.lua
@@ -1,25 +1,34 @@
--- Enable experimental fast lua module loader
-vim.loader.enable()
+vim.loader.enable() -- Enable experimental fast lua module loader
 
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\\\"
 
-require("config")
-
 local utils = require("config.utils")
 local lazypath = utils.ensure_plugin("lazy.nvim", "folke/lazy.nvim", "stable")
-local hotpotpath = utils.ensure_plugin("hotpot.nvim", "rktjmp/hotpot.nvim")
+local hotpotpath = utils.ensure_plugin("hotpot.nvim", "rktjmp/hotpot.nvim", "main")
 
 utils.clean_conflicting_parsers()
 
----@diagnostic disable: undefined-field
-vim.opt.rtp:prepend(hotpotpath)
-vim.opt.rtp:prepend(lazypath)
----@diagnostic enable: undefined-field
+vim.opt.rtp:prepend({ hotpotpath, lazypath }) ---@diagnostic disable-line: undefined-field
 
-require("hotpot")
+require("hotpot").setup()
+
+require("config")
 require("keybindings")
 require("config.autocmds")
 
-require("lazy").setup("plugins")
+local hotpotcontext = assert(require("hotpot.api").context(vim.fn.stdpath("config")))
+
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+        { "rktjmp/hotpot.nvim", version = "^2.0.0" },
+    },
+    performance = {
+        rtp = {
+            paths = { hotpotcontext.locate("destination") },
+        },
+    },
+})
+
 require("nvim-web-devicons").refresh() -- This fixes screwiness with the devicon colors

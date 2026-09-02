d903714a20e1a3ba25883dcdbd656859b10e82cd
Cian Hughes <chughes000@gmail.com>
2026-09-02 11:37:22 +0100

    Revert "YET ANOTHER attempt to fix nvim"
    This reverts commit 4a211bd5e3f10d6305aba7ecac5ed0c33ca9b731.

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.loader.enable()
+
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\\\"
 
@@ -14,7 +16,29 @@ vim.opt.rtp:prepend(hotpotpath)
 vim.opt.rtp:prepend(lazypath)
 ---@diagnostic enable: undefined-field
 
+local original_secure_read = vim.secure.read
+vim.secure.read = function(path) ---@diagnostic disable-line: duplicate-set-field
+    if path:match("%.hotpot%.fnl$") then
+        -- Only auto-trust if it resides in the immutable Nix store or user config directory
+        if path:match("^/nix/store/") or path:match("^/home/cianh/%.config/nvim/") then
+            local file = io.open(path, "r")
+            if file then
+                local content = file:read("*a")
+                file:close()
+                return content
+            end
+        end
+    end
+    return original_secure_read(path)
+end
+
 require("hotpot").setup()
+pcall(function()
+    local ctx = require("hotpot.util").R.Context.new(vim.fn.stdpath("config"))
+    require("hotpot.util").R.Context.sync(ctx)
+end)
+vim.loader.reset()
+
 require("lazy").setup("plugins")
 
 require("keybindings")
